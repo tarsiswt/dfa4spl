ad7dd17d6a94bf00cbb9229759d5d6bb60ab1435
marcinhosbt <marcinhosbt@6c4dff51-b634-49cf-a5fd-882218f1ddba>
2010-11-07 20:08:29 +0000

    Novos métodos na classe LiftedFlowSet
    git-svn-id: https://subversion.assembla.com/svn/cide_ei_rmk@23 6c4dff51-b634-49cf-a5fd-882218f1ddba

--- src/br/ufal/cideei/soot/analyses/LiftedFlowSet.java
+++ src/br/ufal/cideei/soot/analyses/LiftedFlowSet.java
@@ -12,9 +12,9 @@ import soot.toolkits.scalar.FlowSet;
 
 public class LiftedFlowSet<T> extends AbstractFlowSet {
 	
-	Map<Set<String>, FlowSet> map;
+	private Map<Set<String>, FlowSet> map;
 	
-	Set<String> actualConfiguration;
+	private Set<String> actualConfiguration;
 	
 	public LiftedFlowSet() {
 		// TODO Auto-generated constructor stub
@@ -47,20 +47,15 @@ public class LiftedFlowSet<T> extends AbstractFlowSet {
 
 	@Override
 	public void remove(Object object) {
-		// TODO Auto-generated method stub
-		
-		// eh pra remover somente o atual ou de todas as configs???
-		// ou eu pego as configs da unit (object) e removo somente dessas configs?
-		
-//		FeatureTag<T> tag = getFeatureTag(object);
-//		if (tag != null) {
-//			List<Set<String>> taggedFeatures = (List<Set<String>>) tag.getFeatures();
-//			for (Set<String> taggedFeatureSet : taggedFeatures) {
-//				if (map.containsKey(taggedFeatureSet)) {
-//					map.get(taggedFeatureSet).add(object);
-//				}				
-//			}
-//		}
+		FeatureTag<T> tag = getFeatureTag(object);
+		if (tag != null) {
+			List<Set<String>> taggedFeatures = (List<Set<String>>) tag.getFeatures();
+			for (Set<String> taggedFeatureSet : taggedFeatures) {
+				if (map.containsKey(taggedFeatureSet)) {
+					map.get(taggedFeatureSet).remove(object);
+				}
+			}
+		}
 	}
 
 	@Override
@@ -71,18 +66,49 @@ public class LiftedFlowSet<T> extends AbstractFlowSet {
 
 	@Override
 	public List toList() {
-		// TODO Auto-generated method stub
-		return null;
+		FlowSet configurationFlowSet = map.get(actualConfiguration);
+		return configurationFlowSet.toList();
 	}
 
 	public LiftedFlowSet clone() {
+		
 		return null;
+	}
+
+	@Override
+	public void copy(FlowSet dest) {
+		// TODO Auto-generated method stub
+		LiftedFlowSet otherLifted = (LiftedFlowSet) dest;
+		
+		Set<Set<String>> configurations = map.keySet();
+		
+		for (Set<String> configuration : configurations) {
+			actualConfiguration = configuration;
+			
+			FlowSet thisNormal = (FlowSet) map.get(configuration);
+			
+			//copy();
+		}
+	}
+	
+	@Override
+	public void difference(FlowSet other) {
+		LiftedFlowSet otherLifted = (LiftedFlowSet) other;
+		
+		Set<Set<String>> configurations = map.keySet();
+		
+		for (Set<String> configuration : configurations) {
+			actualConfiguration = configuration;
+			
+			FlowSet otherNormal = (FlowSet) otherLifted.map.get(configuration);
+			FlowSet thisNormal = (FlowSet) map.get(configuration);
+			
+			thisNormal.difference(otherNormal);
+		}
 	}
 	
 	@Override
 	public void difference(FlowSet other, FlowSet dest) {
-		// a analise vai chamar esse metodo passando LiftedFlowSet...
-		
 		LiftedFlowSet otherLifted = (LiftedFlowSet) other;
 		LiftedFlowSet destLifted = (LiftedFlowSet) dest;
 		
@@ -95,9 +121,75 @@ public class LiftedFlowSet<T> extends AbstractFlowSet {
 			FlowSet thisNormal = (FlowSet) map.get(configuration);
 			FlowSet destNormal = (FlowSet) destLifted.map.get(configuration);
 			
-			//tarsis: e assim mesmo? dest.metodo(this, outro)??? Nunca sei essa ordem!
-			//olhei isso na API do SOOT, mas queria confirmacao
-			destNormal.difference(thisNormal, otherNormal);
+			thisNormal.difference(otherNormal, destNormal);
+		}
+	}
+
+	@Override
+	public void intersection(FlowSet other) {
+		LiftedFlowSet otherLifted = (LiftedFlowSet) other;
+		
+		Set<Set<String>> configurations = map.keySet();
+		
+		for (Set<String> configuration : configurations) {
+			actualConfiguration = configuration;
+			
+			FlowSet otherNormal = (FlowSet) otherLifted.map.get(configuration);
+			FlowSet thisNormal = (FlowSet) map.get(configuration);
+			
+			thisNormal.intersection(otherNormal);
+		}
+	}
+	
+	@Override
+	public void intersection(FlowSet other, FlowSet dest) {
+		LiftedFlowSet otherLifted = (LiftedFlowSet) other;
+		LiftedFlowSet destLifted = (LiftedFlowSet) dest;
+		
+		Set<Set<String>> configurations = map.keySet();
+		
+		for (Set<String> configuration : configurations) {
+			actualConfiguration = configuration;
+			
+			FlowSet otherNormal = (FlowSet) otherLifted.map.get(configuration);
+			FlowSet thisNormal = (FlowSet) map.get(configuration);
+			FlowSet destNormal = (FlowSet) destLifted.map.get(configuration);
+			
+			thisNormal.intersection(otherNormal, destNormal);
+		}
+	}
+	
+	@Override
+	public void union(FlowSet other) {
+		LiftedFlowSet otherLifted = (LiftedFlowSet) other;
+		
+		Set<Set<String>> configurations = map.keySet();
+		
+		for (Set<String> configuration : configurations) {
+			actualConfiguration = configuration;
+			
+			FlowSet otherNormal = (FlowSet) otherLifted.map.get(configuration);
+			FlowSet thisNormal = (FlowSet) map.get(configuration);
+			
+			thisNormal.union(otherNormal);
+		}
+	}
+	
+	@Override
+	public void union(FlowSet other, FlowSet dest) {
+		LiftedFlowSet otherLifted = (LiftedFlowSet) other;
+		LiftedFlowSet destLifted = (LiftedFlowSet) dest;
+		
+		Set<Set<String>> configurations = map.keySet();
+		
+		for (Set<String> configuration : configurations) {
+			actualConfiguration = configuration;
+			
+			FlowSet otherNormal = (FlowSet) otherLifted.map.get(configuration);
+			FlowSet thisNormal = (FlowSet) map.get(configuration);
+			FlowSet destNormal = (FlowSet) destLifted.map.get(configuration);
+			
+			thisNormal.union(otherNormal, destNormal);
 		}
 	}
 	
