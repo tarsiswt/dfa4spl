9e22b925cf96c81ca813d216909ef4bc003b5672
lfliborio <lfliborio@6c4dff51-b634-49cf-a5fd-882218f1ddba>
2011-05-28 00:16:45 +0000

    Adição dos Visitors correspondentes à seleção de uma feature inteira
    git-svn-id: https://subversion.assembla.com/svn/cide_ei_rmk@90 6c4dff51-b634-49cf-a5fd-882218f1ddba

---
+++ src/br/ufal/cideei/visitors/AllFeatureNodes.java
@@ -0,0 +1,66 @@
+package br.ufal.cideei.visitors;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jface.text.ITextSelection;
+import org.eclipse.jdt.core.dom.ASTVisitor;
+
+import br.ufal.cideei.features.CIDEFeatureExtracterFactory;
+import br.ufal.cideei.features.IFeatureExtracter;
+
+public class AllFeatureNodes extends ASTVisitor{
+
+	private ITextSelection textSelection;
+	private IFile file;
+	private IFeatureExtracter extracter;
+	
+	/** The nodes will be added to this Set as the visitor visits the nodes*/
+	private Set<ASTNode> nodes = new HashSet<ASTNode>();
+	private Set<String> selectionFeatures;
+
+	/**
+	 * Instantiates a new selection nodes visitor.
+	 */
+	@SuppressWarnings("unused")
+	private AllFeatureNodes() {
+	}
+
+	/**
+	 * Instantiates a new selection nodes visitor.
+	 *
+	 * @param textSelection the text selection
+	 */
+	public AllFeatureNodes(ITextSelection textSelection, IFile file, Set<String> features) {
+		extracter = CIDEFeatureExtracterFactory.getInstance().newExtracter();
+		selectionFeatures = features;
+		this.textSelection = textSelection;
+		this.file = file;
+	}
+	
+	/**
+	 * Gets the nodes. 
+	 *
+	 * @return the nodes
+	 */
+	public Set<ASTNode> getNodes(){
+		return nodes;
+	}
+	
+	/**
+	 * Populates the {@link #nodes} Set with the ASTNodes. 
+	 * Use {@link #getNodes()} to retrive the nodes after accepting this visitor to an ASTNode
+	 * @see org.eclipse.jdt.core.dom.ASTVisitor#preVisit(org.eclipse.jdt.core.dom.ASTNode)
+	 */
+	public void preVisit(ASTNode node) {
+		super.preVisit(node);
+		Set<String> featuresNode = extracter.getFeaturesNames(node, file);
+		if (!featuresNode.isEmpty() && selectionFeatures.containsAll(featuresNode)) {
+			nodes.add(node);
+		}
+	}
+	
+}
---
+++ src/br/ufal/cideei/visitors/GetFeatureVisitor.java
@@ -0,0 +1,70 @@
+package br.ufal.cideei.visitors;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jface.text.ITextSelection;
+import org.eclipse.jdt.core.dom.ASTVisitor;
+
+import br.ufal.cideei.features.CIDEFeatureExtracterFactory;
+import br.ufal.cideei.features.IFeatureExtracter;
+
+public class GetFeatureVisitor extends ASTVisitor{
+	/** The text selection. */
+	private ITextSelection textSelection;
+	private IFile file;
+	
+	/** The nodes will be added to this Set as the visitor visits the nodes*/
+	private Set<ASTNode> nodes = new HashSet<ASTNode>();
+	private Set<String> features;
+
+	/**
+	 * Instantiates a new selection nodes visitor.
+	 */
+	@SuppressWarnings("unused")
+	private GetFeatureVisitor() {
+	}
+
+	/**
+	 * Instantiates a new selection nodes visitor.
+	 *
+	 * @param textSelection the text selection
+	 */
+	public GetFeatureVisitor(ITextSelection textSelection, IFile file) {
+		this.textSelection = textSelection;
+		this.file = file;
+	}
+	
+	/**
+	 * Gets the nodes. 
+	 *
+	 * @return the nodes
+	 */
+	public Set<ASTNode> getNodes(){
+		return nodes;
+	}
+	
+	public Set<String> getFeatures(){
+		return features;
+	}
+
+	/**
+	 * Populates the {@link #nodes} Set with the ASTNodes. 
+	 * Use {@link #getNodes()} to retrive the nodes after accepting this visitor to an ASTNode
+	 * @see org.eclipse.jdt.core.dom.ASTVisitor#preVisit(org.eclipse.jdt.core.dom.ASTNode)
+	 */
+	public void preVisit(ASTNode node) {
+		super.preVisit(node);
+		if (node.getStartPosition() >= textSelection.getOffset() && 
+				(node.getStartPosition() + node.getLength()) <= textSelection.getOffset() + textSelection.getLength()) {
+			IFeatureExtracter extracter = CIDEFeatureExtracterFactory.getInstance().newExtracter();
+			features = extracter.getFeaturesNames(node, file);
+			return;
+		}
+	}
+	
+	
+	
+}
